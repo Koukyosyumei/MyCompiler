8e839b4aacb391261d34fd47d3055576ed4c94b6
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-25 21:03:47 +0900

    Update

--- src/generator.hs
+++ src/generator.hs
@@ -3,23 +3,31 @@ module Generator where
 import Parser
 
 data Code = LCODE String | ERROR String deriving(Eq, Show)
+data SFloat = ACT Float | SYM String deriving(Eq, Show)
 
-codeGen :: [(String, [String])] -> [(String, Float)] -> ExprAST -> Code
+type FEnv = [(String, [String])]
+type VEnv = [(String, SFloat)]
 
-codeGen _ _ (NumberExprAST val) = LCODE ((show val))
+_getCode :: (Code, FEnv, VEnv) -> Code
+_getCode (a, b, c) = a
 
-codeGen _ namedValue (VariableExprAST name) = 
+codeGen :: FEnv -> VEnv -> ExprAST -> (Code, FEnv, VEnv)
+
+codeGen fenv venv (NumberExprAST val) = (LCODE (show val), fenv, venv)
+
+codeGen fenv namedValue (VariableExprAST name) = 
     case (lookup name namedValue) of
-        Just val -> LCODE ("float " ++ (show val))
-        Nothing -> ERROR "Unknown variable name"
+        Just (ACT val) -> (LCODE ("float " ++ (show val)), fenv, namedValue)
+        Just (SYM sym) -> (LCODE sym, fenv, namedValue)
+        Nothing -> (ERROR "Unknown variable name", fenv, namedValue)
         
 codeGen funcTable namedValue (BinaryExprAST op lhs rhs) =
     case op of
-        '+' -> fADD lcode rcode "%addtmp"
-        '-' -> fSUB lcode rcode "%subtmp"
-        '*' -> fMUL lcode rcode "%multmp"
-        '<' -> fCmpULT lcode rcode "%cmptmp"
-        _ -> ERROR "invalid binary operator"
+        '+' -> (fADD (_getCode lcode) (_getCode rcode) "%addtmp", funcTable, namedValue ++ [("%addtmp", SYM "%addtmp")])
+        '-' -> (fSUB (_getCode lcode) (_getCode rcode) "%subtmp", funcTable, namedValue)
+        '*' -> (fMUL (_getCode lcode) (_getCode rcode) "%multmp", funcTable, namedValue)
+        '<' -> (fCmpULT (_getCode lcode) (_getCode rcode) "%cmptmp", funcTable, namedValue)
+        _ -> (ERROR "invalid binary operator", funcTable, namedValue)
     where
         lcode = codeGen funcTable namedValue lhs
         rcode = codeGen funcTable namedValue rhs
@@ -27,24 +35,25 @@ codeGen funcTable namedValue (BinaryExprAST op lhs rhs) =
 codeGen funcTable namedValue (CallExprAST fname argExprs) = 
     case (lookup fname funcTable) of
         Just argNames -> if (length argNames /= length argExprs)
-                            then ERROR "Incorrect # arguments passed"
-                            else createCall fname (map (\arg -> codeGen funcTable namedValue arg) argExprs) "calltmp"  
-        Nothing -> ERROR "Unknown functino referenced"    
+                            then (ERROR "Incorrect # arguments passed", funcTable, namedValue)
+                            else (createCall fname (map (\arg -> _getCode (codeGen funcTable namedValue arg)) argExprs) "calltmp",
+                                 funcTable, namedValue)  
+        Nothing -> (ERROR "Unknown functino referenced", funcTable, namedValue)    
 
 codeGen funcTable namedValue (PrototypeAST fname argNames) = 
-    LCODE ("declare float " ++ ("@" ++ fname') ++ ("(" ++ (joinWithCommaStr argNames) ++ ")"))
+    (LCODE ("declare float " ++ ("@" ++ fname') ++ ("(" ++ (joinWithCommaStr argNames) ++ ")")), funcTable, namedValue)
     where
         fname' = if fname == "" then "0" else fname
 
 codeGen funcTable namedValue (FunctionAST prototype body) =
-    case prototypeCODE of
-        LCODE p -> case bodyCODE of
-                    LCODE b -> LCODE (p ++ " {\n"
+    case _getCode prototypeCODE of
+        LCODE p -> case _getCode bodyCODE of
+                    LCODE b -> (LCODE (p ++ " {\n"
                                         ++ "entry:\n"
                                         ++ "\t" ++ b 
-                                        ++ "\n}\n")
-                    ERROR msg -> ERROR ("function body contains the following errors: " ++ msg)
-        ERROR msg -> ERROR ("function declaration contains the following errors: " ++ msg)
+                                        ++ "\n}\n"), funcTable, namedValue)
+                    ERROR msg -> (ERROR ("function body contains the following errors: " ++ msg), funcTable, namedValue)
+        ERROR msg -> (ERROR ("function declaration contains the following errors: " ++ msg), funcTable, namedValue)
     where
         prototypeCODE = codeGen funcTable namedValue prototype
         bodyCODE = codeGen funcTable namedValue body
