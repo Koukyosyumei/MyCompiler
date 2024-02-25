459f8e0c3a843d542ed965b15f1023f722e01c4d
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-25 13:46:01 +0900

    update

--- src/test.hs
+++ src/test.hs
@@ -1,16 +1,24 @@
 import Lexer
 import Parser
 
+check :: (Eq a, Show a) => String -> a -> a -> IO()
+check s c v = do
+    if c == v
+        then putStrLn $ s ++ ": OK" 
+        else putStrLn $ s ++ ": NG (" ++ show c ++ " /= " ++ show v ++ ")"
+
 main :: IO ()
 main = do
     let alpha = "abc123 .456 def abc.123 edf"
         resultAlpha1 = getAlpha alpha 0
         resultAlpha2 = getAlpha alpha 12
-    putStrLn $ show (fst resultAlpha1) ++ ", " ++ show (snd resultAlpha1)
-    putStrLn $ show (fst resultAlpha2) ++ ", " ++ show (snd resultAlpha2)
-    
+    check "test1-1" (fst resultAlpha1) (TokIDENTIFIER "abc123")
+    check "test1-2" (snd resultAlpha1) 6
+    check "test1-3" (fst resultAlpha2) TokDEF
+    check "test1-4" (snd resultAlpha2) 15
+
     let digit = "123.456 def abc0.123 edf"
         resultDigit1 = getDigit digit 0
         resultDigit2 = getDigit digit 15
-    putStrLn $ show (fst resultDigit1) ++ ", " ++ show (snd resultDigit1)
-    putStrLn $ show (fst resultDigit2) ++ ", " ++ show (snd resultDigit2)
+    check "test2-1" (fst resultDigit1) (TokNUMBER 123.456)  
+    check "test2-2" (fst resultDigit2) (TokNUMBER 0.123)
