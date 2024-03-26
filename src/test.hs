73dc7d6c3e0395f4fcd919c713597aa5c92c6ecb
Koukyosyumei <koukyosyumei@hotmail.com>
2024-03-26 21:19:17 +0900

    update test cases

--- src/test.hs
+++ src/test.hs
@@ -42,24 +42,22 @@ main = do
     let source3 = "4+5;"
         top3 = parseTop source3 0 []
         code3 = codeGen [] [] (top3 !! 0)
-    check "test3-3" top3 [FunctionAST (PrototypeAST "" [])
+    check "test3-3-0" top3 [FunctionAST (PrototypeAST "" [])
                                     (BinaryExprAST '+' (NumberExprAST 4.0) (NumberExprAST 5.0))]
-    putStr (code2str (_getCode code3))
+    check "test3-3-1" (code2str (_getCode code3)) "declare double @0() {\nentry:\n\t%addtmp0 = fadd double 4.0, 5.0\n\tret double %addtmp0\n}\n"
 
     let source4 = "def foo(a b) a*a+2*a*b+b*b;"
         top4 = parseTop source4 0 []
         code4 = codeGen [] [] (top4 !! 0)
         top4' = parseTop "def bar(a b) foo(a, 4)+bar(2, b);" 0 []
         code4' = codeGen (_getFEnv code4) (_getVEnv code4) (top4' !! 0)
-    check "test3-4" top4 [FunctionAST (PrototypeAST "foo" ["a","b"]) (BinaryExprAST '+' (BinaryExprAST '+' (BinaryExprAST '*' (VariableExprAST "a") (VariableExprAST "a")) (BinaryExprAST '*' (BinaryExprAST '*' (NumberExprAST 2.0) (VariableExprAST "a")) (VariableExprAST "b"))) (BinaryExprAST '*' (VariableExprAST "b") (VariableExprAST "b")))]
-    putStr (code2str (_getCode code4))
-    -- putStr (show top4')
-    -- putStr (show (_getFEnv code4))
-    putStr (code2str (_getCode code4'))
+    check "test3-4-0" top4 [FunctionAST (PrototypeAST "foo" ["a","b"]) (BinaryExprAST '+' (BinaryExprAST '+' (BinaryExprAST '*' (VariableExprAST "a") (VariableExprAST "a")) (BinaryExprAST '*' (BinaryExprAST '*' (NumberExprAST 2.0) (VariableExprAST "a")) (VariableExprAST "b"))) (BinaryExprAST '*' (VariableExprAST "b") (VariableExprAST "b")))]
+    check "test3-4-1" (code2str (_getCode code4)) "declare double @foo(%a, %b) {\nentry:\n\t%multmp0 = fmul double %a, %a\n\t%multmp1 = fmul double 2.0, %a\n\t%multmp2 = fmul double %multmp1, %b\n\t%addtmp0 = fadd double %multmp0, %multmp2\n\t%multmp3 = fmul double %b, %b\n\t%addtmp1 = fadd double %addtmp0, %multmp3\n\tret double %addtmp1\n}\n"
+    check "test3-4-2" (code2str (_getCode code4')) "declare double @bar(%a, %b) {\nentry:\n\t%calltmp0 = call double @foo(double %a, double 4.0)\n\t%calltmp1 = call double @bar(double 2.0, double %b)\n\t%addtmp2 = fadd double %calltmp0, %calltmp1\n\tret double %addtmp2\n}\n"
 
     let source5 = "def fib(x) if x<3 then 1 else fib(x-1)+fib(x-2);"
         top5 = parseTop source5 0 []
         code5 = codeGen [] [] (top5 !! 0)
-    check "test3-5" top5 [FunctionAST (PrototypeAST "fib" ["x"]) (IfExprAST (BinaryExprAST '<' (VariableExprAST "x") (NumberExprAST 3.0)) (NumberExprAST 1.0) (BinaryExprAST '+' (CallExprAST "fib" [BinaryExprAST '-' (VariableExprAST "x") (NumberExprAST 1.0)]) (CallExprAST "fib" [BinaryExprAST '-' (VariableExprAST "x") (NumberExprAST 2.0)])))]
-    check "test3-6" (code2str (_getCode code5)) "declare double @fib(%x) {\nentry:\n\t%cmptmp0 = fcmp ult double %x, 3.0\n\t%ifcond = %cmptmp0\n\tbr i1 %ifcond, label %then, label %else\n\nthen:\n\tbr label %ifcont\n\nelse:\n\t%calltmp0 = call double @fib(double %subtmp0 = fsub double %x, 1.0)\n\t%calltmp1 = call double @fib(double %subtmp0 = fsub double %x, 2.0)\n\t%addtmp0 = fadd double %calltmp0, %calltmp1\n\tbr label %ifcont\n\nifcont:\n\t%iftmp = phi double [ 1.0, %then ], [ %addtmp0, %else ]\n\tret double %iftmp\n}\n"
+    check "test3-5-0" top5 [FunctionAST (PrototypeAST "fib" ["x"]) (IfExprAST (BinaryExprAST '<' (VariableExprAST "x") (NumberExprAST 3.0)) (NumberExprAST 1.0) (BinaryExprAST '+' (CallExprAST "fib" [BinaryExprAST '-' (VariableExprAST "x") (NumberExprAST 1.0)]) (CallExprAST "fib" [BinaryExprAST '-' (VariableExprAST "x") (NumberExprAST 2.0)])))]
+    check "test3-5-1" (code2str (_getCode code5)) "declare double @fib(%x) {\nentry:\n\t%cmptmp0 = fcmp ult double %x, 3.0\n\t%ifcond = %cmptmp0\n\tbr i1 %ifcond, label %then, label %else\n\nthen:\n\tbr label %ifcont\n\nelse:\n\t%calltmp0 = call double @fib(double %subtmp0 = fsub double %x, 1.0)\n\t%calltmp1 = call double @fib(double %subtmp0 = fsub double %x, 2.0)\n\t%addtmp0 = fadd double %calltmp0, %calltmp1\n\tbr label %ifcont\n\nifcont:\n\t%iftmp = phi double [ 1.0, %then ], [ %addtmp0, %else ]\n\tret double %iftmp\n}\n"
 
